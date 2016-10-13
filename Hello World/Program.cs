492bd724422370aca75e947f7b2f115d25a9e2a0
Sinisa Vojtek <sinisa.vojtek@vsite.hr>
2016-10-13 20:17:45 +0200

    Unešene izmjene....

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            int a = 14;
+            double d = 3.14;
+            Console.WriteLine("{0}, {1}",a,d);
             Console.WriteLine("Zdravo");
+            Console.ReadKey();
         }
     }
 }
